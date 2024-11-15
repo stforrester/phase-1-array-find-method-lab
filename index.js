37000c20fab6ec5567568874602f6dd4a805e824
Stuart Forrester <stforrester@gmail.com>
2024-11-15 02:35:23 -0600

    complete lab in 2 ways

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+
+/*function superbowlWin(array) {
+    const callBack = function (element) {
+        return element.result === "W";
+    }
+    
+    const responseVar = array.find(callBack);
+
+    if (responseVar === undefined) {
+        return responseVar;
+    }
+    else {
+        return responseVar.year;
+    }
+};*/
+
+function superbowlWin(array) {
+    const response = array.find((element) => {
+        return element.result === "W";
+    });
+
+    if (response === undefined) {
+        return response;
+    }
+    else {
+        return response.year;
+    }
+}
